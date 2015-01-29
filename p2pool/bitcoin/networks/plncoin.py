524ddcc90a1957cc744c724991225bcf6733df88
Rav3n_pl <rav3n.pl@gmail.com>
2015-01-29 12:48:41 +0100

    Added working block explorer for PLNc

--- p2pool/bitcoin/networks/plncoin.py
+++ p2pool/bitcoin/networks/plncoin.py
@@ -22,9 +22,9 @@ SYMBOL = 'PLNc'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'PlnCoin') 
 		if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/PlnCoin/') 
 		if platform.system() == 'Darwin' else os.path.expanduser('~/.plncoin'), 'plncoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://altexplorer.net/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'http://altexplorer.net/address/'
-TX_EXPLORER_URL_PREFIX = 'http://altexplorer.net/tx/'
+BLOCK_EXPLORER_URL_PREFIX = 'http://explorer.zaplnc.pl/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://explorer.zaplnc.pl/address/'
+TX_EXPLORER_URL_PREFIX = 'http://explorer.zaplnc.pl/tx/'
 SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
